709c21a8dca624f64a03eb7e9e04f3e20dcfad7a
xHexed <minhquanpct7b@gmail.com>
2020-03-18 23:14:42 +0800

    Now using faster output method, improved CommandListener, tiny fixes

--- src/main/java/com/mc3f/amcaddon/CommandListener.java
+++ src/main/java/com/mc3f/amcaddon/CommandListener.java
@@ -1,14 +1,15 @@
 package com.mc3f.amcaddon;
 
+import it.unimi.dsi.fastutil.io.FastBufferedOutputStream;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,31 +17,29 @@ import java.util.Date;
 public class CommandListener implements Listener {
     private void writefile(final String command, final String sender) {
         final File file = new File("plugins/AdvancedMonthlyCrates", "log.txt");
-        for (final String amc : AmcAddon.getAmcCommands()) {
-            if (command.startsWith(amc)) {
-                try {
-                    if (!file.exists())
-                        file.createNewFile();
-                    final PrintWriter printWriter = new PrintWriter(new FileWriter("plugins/AdvancedMonthlyCrates/log.txt", true));
-                    printWriter.println(new SimpleDateFormat("dd/MM/yyyy ss:mm:HH").format(new Date()) + " " + sender + ": " + command);
-                    printWriter.close();
-                }
-                catch (final IOException ignored) { }
-                return;
-            }
+        try (final OutputStream out = new FastBufferedOutputStream(new FileOutputStream("plugins/AdvancedMonthlyCrates/log.txt", true))) {
+            if (!file.exists())
+                file.createNewFile();
+            out.write((new SimpleDateFormat("dd/MM/yyyy ss:mm:HH").format(new Date()) + " " + sender + ": " + command + "\n").getBytes());
+            out.flush();
+        }
+        catch (final IOException e) {
+            e.printStackTrace();
         }
     }
 
     @EventHandler
     public void onPlayerCommand(final PlayerCommandPreprocessEvent e) {
-        if (e.getMessage().length() == 1 || !e.getPlayer().isOp()) return;
-        writefile(e.getMessage().substring(1).trim(), e.getPlayer().getName());
+        if (!e.getPlayer().isOp() || e.getMessage().length() == 1) return;
+        final String command = e.getMessage().substring(1).trim();
+        if (AmcAddon.getAmcCommands().stream().noneMatch(command::startsWith)) return;
+        writefile(command, e.getPlayer().getName());
     }
 
     @EventHandler
     public void onConsoleCommand(final ServerCommandEvent e) {
         final String command = e.getCommand().trim();
-        if (!command.startsWith("amc") || !command.startsWith("pouch giveall")) return;
+        if (AmcAddon.getAmcCommands().stream().noneMatch(command::startsWith)) return;
         writefile(command, "Console");
     }
 }
